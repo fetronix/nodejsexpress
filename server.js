a2353e4459f51560ac67e738a558cc5b58600e29
Fidelis Mwendwa Nzomo <fidelism99@gmail.com>
2023-10-28 01:31:18 +0300

    installing mongoos server

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("Hello world")
+const express = require('express')
+const mongoose = require('mongoose')
+
+const url ="mongodb://localhost:27017/persons"
+const port = 3000;
+const app = express()
+
+mongoose.connect(url,{})
+    .then(result => console.log("database connected"))
+    .catch(error=> console.log(err))
+
+app.get("/",(req,res)=>{
+    res.send("<h1>hello from node js app</h1>")
+})
+
+app.listen(port,() => {
+    console.log("server is running at port " + port)
+})
